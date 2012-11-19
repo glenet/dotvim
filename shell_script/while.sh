62ce373798d1b1e312d70ab1b75a1cdf81c7c002
Browny Lin <browny_lin@htc.com>
2012-11-19 16:48:06 +0800

    vimrc

--- shell_script/while.sh
+++ shell_script/while.sh
@@ -5,9 +5,9 @@ c=1
 while [ $c -le 1000 ]
 do
 	adb shell "echo 4 > /sys/devices/tegradc.0/graphics/fb0/blank"
-	sleep 0.5
+	sleep 1
 	adb shell "echo 0 > /sys/devices/tegradc.0/graphics/fb0/blank"
-	sleep 0.5
+	sleep 1
 	(( c++ ))
 done
 
